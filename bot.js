2e7eebbe924e5a4b3c2b27a789f980dd35cf2615
Daniel Branham <idanielbranham@gmail.com>
2018-08-01 21:03:06 -0400

    small things
    small things

--- bot.js
+++ bot.js
@@ -12,19 +12,19 @@ client.on("ready", () => { //runs if login was successful
     console.log(`Bot has started, with ${client.users.size} users, in ${client.channels.size} channels of ${client.guilds.size} guilds.`); 
     // Example of changing the bot's playing game to something useful. `client.user` is what the
     // docs refer to as the "ClientUser".
-    client.user.setActivity(`Serving ${client.guilds.size} server`);
+    client.user.setActivity(`Playing Fortnite with ${client.guilds.size} friends`);
 });
 
 client.on("guildCreate", guild => {
     // This event triggers when the bot joins a guild.
     console.log(`New guild joined: ${guild.name} (id: ${guild.id}). This guild has ${guild.memberCount} members!`);
-    client.user.setActivity(`Serving ${client.guilds.size} server`);
+    client.user.setActivity(`Playing Fortnite with ${client.guilds.size} friends`);
 });
 
 client.on("guildDelete", guild => {
     // this event triggers when the bot is removed from a guild.
     console.log(`I have been removed from: ${guild.name} (id: ${guild.id})`);
-    client.user.setActivity(`Serving ${client.guilds.size} server`);
+    client.user.setActivity(`Playing Fortnite with ${client.guilds.size} friends`);
 });
 
 console.log("* \r");
